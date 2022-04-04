32e05d386802224bea8bf024af04a8684b1ae741
Dmitrijs Trizna <dmitrijs.trizna@avast.com>
2022-04-04 16:50:10 +0300

    updated tokenization flow

--- slp.py
+++ slp.py
@@ -1,5 +1,6 @@
 import numpy as np
 import bashlex
+import logging
 import re
 from collections import Counter, defaultdict
 from sklearn.preprocessing import LabelEncoder
@@ -9,13 +10,19 @@ from scipy.sparse import csr_matrix
 class ShellTokenizer():
     def __init__(self, debug=False, verbose=False):
         self.ERR = 0
-        self.debug = debug
         self.verbose = verbose
         self.data = None
-        self.global_counter = None
-        self.corpus = None
-        self.tokenized_node = None
+        self.global_counter = Counter()
+        self.tokenized_corpus = []
 
+        # setup logging
+        level = logging.INFO if verbose else logging.WARNING
+        level = logging.DEBUG if debug else level
+        logconfig = {
+            "level": level,
+            "format": "%(asctime)-15s %(name)-5s %(levelname)-8s %(message)s"
+        }
+        logging.basicConfig(**logconfig)
 
     def _bashlex_wrapper(self, command):
         try:
@@ -31,15 +38,17 @@ class ShellTokenizer():
                 rude_parse.append(element)
             return rude_parse
 
-
     def _update_objects(self, counters, tokens):
-        self.global_counter += counters
-        self.tokenized_node.extend(tokens)
-
+        try:
+            self.global_counter += counters
+            self.tokenized_node.extend(tokens)
+        except Exception as ex:
+            print(ex)
+            import pdb;pdb.set_trace()
 
     # self-calling iterative function
     def _iterate_bashlex(self, bashlex_object):
-        local_cntr = Counter()
+        local_counter = Counter()
         local_tokens = []
 
         # handling different bashlex object types into elements of Counter()
@@ -52,7 +61,6 @@ class ShellTokenizer():
         
             if 'command' in object_methods:
                 self._update_objects(*self._iterate_bashlex(bashlex_object.command))
-            
             elif 'list' in object_methods:
                 for element in bashlex_object.list:
                     self._update_objects(*self._iterate_bashlex(element))
@@ -63,10 +71,9 @@ class ShellTokenizer():
                     for part in bashlex_object.parts:
                         self._update_objects(*self._iterate_bashlex(part))
                 else:
-                    local_cntr, local_tokens = self._parse_word(bashlex_object)
+                    local_counter, local_tokens = self._parse_word(bashlex_object)
             elif 'word' in object_methods:
-                local_cntr, local_tokens = self._parse_word(bashlex_object)
-            
+                local_counter, local_tokens = self._parse_word(bashlex_object)
             # Working on default specific types
             elif 'value' in object_methods:
                 self.global_counter[bashlex_object.value] += 1
@@ -81,15 +88,12 @@ class ShellTokenizer():
                 self.global_counter[bashlex_object.type] += 1
                 self.tokenized_node.append(bashlex_object.type)
             else:
-                if self.debug:
-                    print("[DEBUG] Weird case - not parsed correctly:", bashlex_object)
-                    #import pdb; pdb.set_trace()
+                logging.info(f"[DEBUG] Weird case - not parsed correctly: {bashlex_object}")
         
-        return local_cntr, local_tokens
-
+        return local_counter, local_tokens
 
     def _parse_word(self, bashlex_object):
-        local_cntr = Counter()
+        local_counter = Counter()
         local_tokens = []
         size = 1
         # Trying to parse input object again
@@ -107,77 +111,64 @@ class ShellTokenizer():
         if size > 1:
             for node in p:
                 self._update_objects(*self._iterate_bashlex(node))
-        
         # if no new elements - improve bashlex parsing of flags w/ '='
         elif '=' in bashlex_object.word and \
         '==' not in bashlex_object.word:
             l = bashlex_object.word.split('=')
             # special case
             if 'chmod' in bashlex_object.word.lower():
-                local_cntr[l[0]] += 1
-                local_cntr['='.join(l[1:])] += 1
+                local_counter[l[0]] += 1
+                local_counter['='.join(l[1:])] += 1
                 local_tokens.extend([l[0], '='.join(l[1:])])
             # standard 'flag=value' case
             elif len(l) == 2:
-                local_cntr[l[0]] += 1
-                local_cntr[l[1]] += 1
+                local_counter[l[0]] += 1
+                local_counter[l[1]] += 1
                 local_tokens.extend([l[0], l[1]])
             # weird combination - add as is
             else:
-                local_cntr[bashlex_object.word] += 1
+                local_counter[bashlex_object.word] += 1
                 local_tokens.append(bashlex_object.word)
- 
         # bashlex parsing correct
         else:
-            local_cntr[bashlex_object.word] += 1
+            local_counter[bashlex_object.word] += 1
             local_tokens.append(bashlex_object.word)
 
-        return local_cntr, local_tokens
+        return local_counter, local_tokens
     
-
-    def tokenize(self, data):
-        self.data = data
-        self.global_counter = Counter()
-        self.corpus = []
-        
-        l = len(data)
-        for i,command in enumerate(data):
-            self.tokenized_command = []
-            
+    def tokenize_corpus(self, corpus):
+        l = len(corpus)
+        for i, command in enumerate(corpus):
             if self.verbose:
                 print(f"[*] Parsing in process: {i+1}/{l}", end="\r")
             
-            # bashlex wrapper returns list
-            nodes = self._bashlex_wrapper(command)
+            self.tokenized_corpus.append(self.tokenize_command(command, i=i))
+        return self.tokenized_corpus, self.global_counter
+    
+    def tokenize_command(self, command, i="*"):
+        tokenized_command = []
+        # bashlex wrapper returns list
+        nodes = self._bashlex_wrapper(command)
+        # if it consists of strings - then parsing failed 
+        # and rude splitting is performed, add splitted command to corpus as is
+        if isinstance(nodes[0], str):
+            logging.debug(f"[{i}] 'bashlex' failed, regex tokenization:\n\t{command}\n\t{nodes}\n")
             
-            # if it consists of strings - then parsing failed 
-            # and rude splitting is performed, add splitted command to corpus as is
-            if isinstance(nodes[0], str):
-                if self.debug:
-                    print(f"[DEBUG] bashlex failed, manual parsing case:\n\t{command}\n\t{nodes}\n")
-                    #import pdb; pdb.set_trace()
-                self.tokenized_command.extend(nodes)
-                self.global_counter += Counter(nodes)
-            
-            # if it consists of bashlex nodes - parsing suceeded
-            elif isinstance(nodes[0], bashlex.ast.node):
-                for node in nodes:
-                    self.tokenized_node = []
-                    self._update_objects(*self._iterate_bashlex(node))
-                    self.tokenized_command.extend(self.tokenized_node)
-            
-            # weird ..
-            else:
-                print(f"[-] Unexpected return type from _bashlex_wrapper for command:\n\t{command}")
-                if self.debug:
-                    import pdb; pdb.set_trace()
-                raise TypeError
-
-            self.corpus.append(self.tokenized_command)
-        print()
+            tokenized_command.extend(nodes)
+            self.global_counter += Counter(nodes)
+        # if it consists of bashlex nodes - parsing suceeded
+        elif isinstance(nodes[0], bashlex.ast.node):
+            for node in nodes:
+                self.tokenized_node = []
+                self._update_objects(*self._iterate_bashlex(node))
+                tokenized_command.extend(self.tokenized_node)
+        else:
+            logging.info(f"[-] Unexpected return type from 'bashlex', skipping command:\n\t{command}")
         
-        return self.corpus, self.global_counter
+        return tokenized_command
 
+    def tokenize(self, data):
+        return self.tokenize_corpus(data)
 
 
 class ShellEncoder():
@@ -211,9 +202,8 @@ class ShellEncoder():
 
         # TF-IDF
         tfidf = np.zeros((self.l, self.top_tokens))
+        logging.info(f"[!] Starting TF-IDF encoding!")
         for i,cmd in enumerate(self.corpus):
-            if self.verbose:
-                print(f"\n[*] TF-IDF encoding: {i+1}/{self.l}", end="\r")
             omc = Counter(cmd)
             tf = defaultdict(float)            
             for token in set(cmd):
@@ -225,11 +215,11 @@ class ShellEncoder():
                 else:
                     # token not in top token list, omit
                     pass
+        logging.info(f"[!] TF-IDF encoding finished!")
         return csr_matrix(tfidf)
     
     
-    def labels(self, pad_width=None):
-                    
+    def labels(self, pad_width=None):          
         self.top_token_list = [x[0] for x in self.most_common]
         local_corpus = self.corpus.copy()
 
@@ -240,25 +230,24 @@ class ShellEncoder():
         max_pad = pad_width if pad_width else np.max([len(x) for x in local_corpus])
         output = np.zeros((len(local_corpus), max_pad), dtype= np.int)
         
+        logging.info(f"[!] Starting Label encoding!")
         for i,x in enumerate(local_corpus):
-            if self.verbose:
-                print(f"\n[*] Label encoding: {i+1}/{self.l}", end="\r")
-            
             z = np.zeros(max_pad)
             if max_pad > x.shape[0]:
                 z[:x.shape[0]] = x
             else:
                 z = x[:max_pad]
             output[i] = z
+        logging.info(f"[!] Label encoding finished!")
         return csr_matrix(output)
         
     
     def onehot(self):
         output = np.zeros(shape=(self.l, self.top_tokens), dtype=int)
+        logging.info(f"[!] Starting One-Hot encoding!")
         for i, cmd in enumerate(self.corpus):
-            if self.verbose:
-                print(f"\n[*] One-Hot encoding: {i+1}/{self.l}", end="\r")
             for j,token in enumerate(self.top_token_list):
                 if token in cmd:
                     output[i,j] = 1
+        logging.info(f"[!] One-Hot encoding finished!")
         return csr_matrix(output)
